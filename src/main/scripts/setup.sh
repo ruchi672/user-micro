877fa06fb3675801ca8c1e86482044dfa4a8c778
ruchi672 <61773688+ruchi672@users.noreply.github.com>
2020-09-22 14:22:27 +0530

    Update setup.sh

--- src/main/scripts/setup.sh
+++ src/main/scripts/setup.sh
@@ -1,7 +1,5 @@
-microk8s.kubectl create -f deployment-todo.yml
 microk8s.kubectl create -f deployment-user.yml
 
-microk8s.kubectl create -f service-todo.yml
 microk8s.kubectl create -f service-user.yml
 
 microk8s.kubectl create -f ingress.yml
